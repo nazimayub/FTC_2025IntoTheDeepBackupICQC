23b493988be0854c9906df37bbb6a5ec8408de49
Isaaq (IK_Knight) <144549093+Zayan-Hassan@users.noreply.github.com>
2024-09-07 21:56:18 -0500

    patyh

--- MeepMeepRun/src/main/java/com/example/meepmeeprun/MeepMeepTesting.java
+++ MeepMeepRun/src/main/java/com/example/meepmeeprun/MeepMeepTesting.java
@@ -1,7 +1,11 @@
 package com.example.meepmeeprun;
 
+import com.acmerobotics.roadrunner.MappedPosePath;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Trajectory;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -19,24 +23,76 @@ public class MeepMeepTesting {
         System.setProperty("sun.java2d.opengl", "true");
         MeepMeep meepMeep = new MeepMeep(800);
         // custom image stuff
-
-
-
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(120, 120, Math.toRadians(180), Math.toRadians(180), 11) // 11x11 tw
+                .setConstraints(150, 150, Math.toRadians(180), Math.toRadians(180), 11) // 11x11 tw
                 .build();
+        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(150, 150, Math.toRadians(180), Math.toRadians(180), 11) // 11x11 tw
+                .build();
+        String path = "1+4zp2";
+        boolean run = true;
+        if(path.equals("1+5zp1") || run){
+            myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(20, 60, Math.toRadians(270)))
+                    .strafeToConstantHeading(new Vector2d(10,35))
+                    .waitSeconds(1)
+                    .strafeToConstantHeading(new Vector2d(15, 45))
+                    .strafeToLinearHeading(new Vector2d(40, 45), Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(47, 35), Math.toRadians(90))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(55, 55), Math.toRadians(45))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(57, 35), Math.toRadians(90))
+                    .waitSeconds(2)
+                    .strafeToLinearHeading(new Vector2d(55, 55), Math.toRadians(45))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(55, 24), Math.toRadians(180))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(55, 55), Math.toRadians(45))
+                    .waitSeconds(1)
+                    .strafeToConstantHeading(new Vector2d(55, 40))
+                    .strafeToLinearHeading(new Vector2d(-47, 35), Math.toRadians(90))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(55, 55), Math.toRadians(45))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(-57, 35), Math.toRadians(90))
+                    .waitSeconds(2.3)
+                    .strafeToLinearHeading(new Vector2d(20, 60), Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50, 60), Math.toRadians(0))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(10, 35), Math.toRadians(90))
+                    .build());
+        }
+        if (path.equals("1+4zp2") || run){
+            myBot2.runAction(myBot.getDrive().actionBuilder(new Pose2d(-20, 60, Math.toRadians(270)))
+                    .strafeToConstantHeading(new Vector2d(-10,35))
+                    .waitSeconds(1)
+                    .strafeToConstantHeading(new Vector2d(-36, 36))
+                    .strafeToLinearHeading(new Vector2d(-48, -13), Math.toRadians(90))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(-40,60),Math.toRadians(0))
+                    .strafeToConstantHeading(new Vector2d(50,60))
+                    .waitSeconds(1)
+                    .strafeToConstantHeading(new Vector2d(-36, 36))
+                    .strafeToLinearHeading(new Vector2d(-55, -13), Math.toRadians(90))
+                    .waitSeconds(2)
+                    .strafeToLinearHeading(new Vector2d(-40,60),Math.toRadians(0))
+                    .strafeToConstantHeading(new Vector2d(50,60))
+                    .waitSeconds(1)
+                    .strafeToConstantHeading(new Vector2d(-50, 60))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(-55, -24), Math.toRadians(0))
+                    .waitSeconds(1)
+                    .strafeToConstantHeading(new Vector2d(-22, 60))
+                    .strafeToConstantHeading(new Vector2d(50, 60))
+                    .waitSeconds(1)
+                    .strafeToConstantHeading(new Vector2d(50, 40))
+                    .strafeToLinearHeading(new Vector2d(-10, 35), Math.toRadians(90))
+                    .build());
+        }
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
         Image img = null;
         try {
             img = ImageIO.read(new File("field.png"));
@@ -45,6 +101,7 @@ public class MeepMeepTesting {
 
         meepMeep.setBackground(img)
                 .addEntity(myBot)
+                .addEntity(myBot2)
                 .start();
     }
 }
