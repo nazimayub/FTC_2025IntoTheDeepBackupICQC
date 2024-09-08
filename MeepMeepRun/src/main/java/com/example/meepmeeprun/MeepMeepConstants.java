9a7ad555a807c6b202cd4e49e4048b7c092d6c55
IK_Knight <76535471+IKKNIGHT@users.noreply.github.com>
2024-09-08 16:24:43 -0500

    Completed Auto paths on MeepMeep

--- MeepMeepRun/src/main/java/com/example/meepmeeprun/MeepMeepConstants.java
+++ MeepMeepRun/src/main/java/com/example/meepmeeprun/MeepMeepConstants.java
@@ -6,8 +6,14 @@ public class MeepMeepConstants {
     public static final Pose2d BLUE_1 = new Pose2d(20,60,Math.toRadians(270));
     public static final Pose2d BLUE_2 = new Pose2d(-20,60,Math.toRadians(270));
 
+    public static final Pose2d RED_1 = new Pose2d(-20,-60,Math.toRadians(90));
+    public static final Pose2d RED_2 = new Pose2d(20,-60,Math.toRadians(90));
+
+
     public static final String B1 = "B1";
     public static final String B2 = "B2";
+    public static final String R1 = "R1";
+    public static final String R2 = "R2";
     public static final double MAXVEL = 120;
     public static final double MAXACCEL = 120;
     public static final double MAXANGVEL = Math.toRadians(180);
--- MeepMeepRun/src/main/java/com/example/meepmeeprun/MeepMeepTesting.java
+++ MeepMeepRun/src/main/java/com/example/meepmeeprun/MeepMeepTesting.java
@@ -16,7 +16,7 @@ import javax.imageio.ImageIO;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        String path = "B1";
+        String path = "R2";
 
         System.setProperty("sun.java2d.opengl", "true");
         MeepMeep meepMeep = new MeepMeep(800);
@@ -36,6 +36,56 @@ public class MeepMeepTesting {
                     .strafeToLinearHeading(new Vector2d(50,52),Math.toRadians(45)) // score hb
                     .strafeToLinearHeading(new Vector2d(58,25),Math.toRadians(180))
                     .strafeToLinearHeading(new Vector2d(50,52),Math.toRadians(45))
+                    .strafeToLinearHeading(new Vector2d(0,34),Math.toRadians(270))
+                    .build());
+        }
+        if (path.equalsIgnoreCase(MeepMeepConstants.B2)){
+            myBot.runAction(myBot.getDrive().actionBuilder(MeepMeepConstants.BLUE_2)
+                    .strafeToLinearHeading(new Vector2d(-9,34),Math.toRadians(270))
+                    .strafeToLinearHeading(new Vector2d(-11,40),Math.toRadians(270))
+                    .strafeToLinearHeading(new Vector2d(-44,40),Math.toRadians(180+45+18)) //-30,60
+                    .strafeToLinearHeading(new Vector2d(-30,60),Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50,52),Math.toRadians(45))
+                    .strafeToLinearHeading(new Vector2d(-30,60),Math.toRadians(180+45+18)) //-55,33
+                    .strafeToLinearHeading(new Vector2d(-55,36),Math.toRadians(180+45+18))
+                    .strafeToLinearHeading(new Vector2d(-30,60),Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50,52),Math.toRadians(45))
+                    .strafeToLinearHeading(new Vector2d(-30,60),Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(-57,26),Math.toRadians(180))
+                    .strafeToLinearHeading(new Vector2d(-30,60),Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50,52),Math.toRadians(45))
+                    .strafeToLinearHeading(new Vector2d(-9,34),Math.toRadians(270))
+                    .build());
+        }
+        if (path.equalsIgnoreCase(MeepMeepConstants.R1)){
+            myBot.runAction(myBot.getDrive().actionBuilder(MeepMeepConstants.RED_1) //270 ->90 180->0
+                    .strafeToLinearHeading(new Vector2d(0,-34),Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(0,-40),Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(-48,-34),Math.toRadians(270))
+                    .strafeToLinearHeading(new Vector2d(-50,-52),Math.toRadians(270-45)) // score hb
+                    .strafeToLinearHeading(new Vector2d(-54,-34),Math.toRadians(270+45))
+                    .strafeToLinearHeading(new Vector2d(-50,-52),Math.toRadians(270-45)) // score hb
+                    .strafeToLinearHeading(new Vector2d(-58,-25),Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(-50,-52),Math.toRadians(270-45))
+                    .strafeToLinearHeading(new Vector2d(0,-34),Math.toRadians(90))
+                    .build());
+        }
+        if (path.equalsIgnoreCase(MeepMeepConstants.R2)){ //270 ->90 180->0
+            myBot.runAction(myBot.getDrive().actionBuilder(MeepMeepConstants.RED_2)
+                    .strafeToLinearHeading(new Vector2d(9,-34),Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(11,-40),Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(44,-40),Math.toRadians(180+45+18)) //-30,60
+                    .strafeToLinearHeading(new Vector2d(30,-60),Math.toRadians(180))
+                    .strafeToLinearHeading(new Vector2d(-50,-52),Math.toRadians(225))
+                    .strafeToLinearHeading(new Vector2d(30,-60),Math.toRadians(180+45+18)) //-55,33
+                    .strafeToLinearHeading(new Vector2d(55,-36),Math.toRadians(180+45+18))
+                    .strafeToLinearHeading(new Vector2d(30,-60),Math.toRadians(180))
+                    .strafeToLinearHeading(new Vector2d(-50,-52),Math.toRadians(225))
+                    .strafeToLinearHeading(new Vector2d(30,-60),Math.toRadians(180))
+                    .strafeToLinearHeading(new Vector2d(57,-26),Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(30,-60),Math.toRadians(180))
+                    .strafeToLinearHeading(new Vector2d(-50,-52),Math.toRadians(225))
+                    .strafeToLinearHeading(new Vector2d(9,-34),Math.toRadians(90))
                     .build());
         }
 
