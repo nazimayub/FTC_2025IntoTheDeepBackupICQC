7e83dd6d1cf5c6ced1b8dec3298d2f0ba91e5fd0
Zayan-Hassan <144549093+Zayan-Hassan@users.noreply.github.com>
2024-09-08 13:40:05 -0500

    new path

--- MeepMeepRun/src/main/java/com/example/meepmeeprun/MeepMeepTesting.java
+++ MeepMeepRun/src/main/java/com/example/meepmeeprun/MeepMeepTesting.java
@@ -37,6 +37,70 @@ public class MeepMeepTesting {
         if(path.equals("1+5zp1") || run){
             myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(20, 60, Math.toRadians(270)))
                     .strafeToConstantHeading(new Vector2d(10,35))
+                        .waitSeconds(1)
+                    .strafeToConstantHeading(new Vector2d(15, 45))
+                    .strafeToLinearHeading(new Vector2d(40, 45), Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(47, 35), Math.toRadians(90))
+                        .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(55, 55), Math.toRadians(45))
+                        .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(48, 36), Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(-36, 36), Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(-49, -13), Math.toRadians(90))
+                        .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(-36, 60), Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50, 60), Math.toRadians(0))
+                        .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(56, 24), Math.toRadians(0))
+                        .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(60, 50), Math.toRadians(90))
+                        .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(50, 36), Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(-60, 36), Math.toRadians(0))
+                        .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(-36, 60), Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50, 60), Math.toRadians(0))
+                        .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(48, 36), Math.toRadians(270))
+                    .strafeToLinearHeading(new Vector2d(-10, 36), Math.toRadians(270))
+                    .build());
+        }
+        if (path.equals("1+4zp2") || run){
+            myBot2.runAction(myBot.getDrive().actionBuilder(new Pose2d(-20, 60, Math.toRadians(270)))
+                    .strafeToConstantHeading(new Vector2d(-10,35))
+                    .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(-36, 36), Math.toRadians(270))
+                    .strafeToLinearHeading(new Vector2d(-49, 36), Math.toRadians(90))
+                            .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(0, 60), Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50, 60), Math.toRadians(0))
+                            .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(60, 36), Math.toRadians(90))
+                            .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(55, 55), Math.toRadians(45))
+                            .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(36, 36), Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(-36, 36), Math.toRadians(90))
+                    .strafeToLinearHeading(new Vector2d(60, -13), Math.toRadians(90))
+                            .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(-36, 60), Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50, 60), Math.toRadians(0))
+                            .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(36, 36), Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(-36, 36), Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(-60, -24), Math.toRadians(0))
+                            .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(-36, 60), Math.toRadians(0))
+                    .strafeToLinearHeading(new Vector2d(50, 60), Math.toRadians(0))
+                            .waitSeconds(1)
+                    .strafeToLinearHeading(new Vector2d(10, 36), Math.toRadians(270))
+
+                    .build());
+        }
+        /*
+        if(path.equals("1+5zp1") || run){
+            myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(20, 60, Math.toRadians(270)))
+                    .strafeToConstantHeading(new Vector2d(10,35))
                     .waitSeconds(1)
                     .strafeToConstantHeading(new Vector2d(15, 45))
                     .strafeToLinearHeading(new Vector2d(40, 45), Math.toRadians(90))
@@ -92,7 +156,7 @@ public class MeepMeepTesting {
                     .strafeToLinearHeading(new Vector2d(-10, 35), Math.toRadians(90))
                     .build());
         }
-
+*/
         Image img = null;
         try {
             img = ImageIO.read(new File("field.png"));
