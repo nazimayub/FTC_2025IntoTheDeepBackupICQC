faed39e4792642ab7b090514c3f0e95bc78a54c0
IK_Knight <76535471+IKKNIGHT@users.noreply.github.com>
2024-09-09 20:05:04 -0500

    ss

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotContainer.java
@@ -67,7 +67,6 @@ public class RobotContainer extends CommandOpMode {
                 telemetry.addLine("Red Selected!");
                 isAllianceBlue = false;
             }
-            telemetry.update();
             telemetry.clearAll();
         }
     }
