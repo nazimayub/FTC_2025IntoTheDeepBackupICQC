884498c20138c73caf3a32656bbec94073a77e7b
NoahMarvelsofMAS <88335646+NoahMarvelsofMAS@users.noreply.github.com>
2024-10-19 15:09:45 -0500

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommand.java
@@ -8,15 +8,23 @@ import org.firstinspires.ftc.teamcode.subsystems.Drive;
 public class DriveCommand extends CommandBase {
     @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
     private final Drive drive;
+    double x,y,theta,tolerance,power;
+    boolean done = false;
 
-    public DriveCommand(Drive drive) {
+    public DriveCommand(Drive drive,double x,double y,double theta,double tolerance) {
         this.drive=drive;
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(drive);
     }
     @Override
     public void execute(){
-
+        done = false;
+        drive.driveToPosition(x,y,theta,power,tolerance); // here we just move...
+        done = true;
+    }
+    @Override
+    public boolean isFinished(){
+        return done;
     }
 
 
