aed02eb9607287553149763343d0e5178a64c55f
NoahMarvelsofMAS <88335646+NoahMarvelsofMAS@users.noreply.github.com>
2024-10-19 14:56:59 -0500

    Update DriveCommand.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveCommand.java
@@ -9,12 +9,7 @@ public class DriveCommand extends CommandBase {
     @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
     private final Drive drive;
 
-    /**
-     * Creates a new ExampleCommand.
-     *
-     * @param subsystem The subsystem used by this command.
-     */
-    public ExampleCommand(Drive drive) {
+    public DriveCommand(Drive drive) {
         this.drive=drive;
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(drive);
