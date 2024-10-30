2788375b1f201ea9a5039cd10ac631b293ca8a81
Abdullah Ahmed <abdullahahmedpk18@gmail.com>
2024-10-29 19:31:10 -0500

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -13,12 +13,10 @@ public class Constants {
     //Names for motors and servos
     public static final String arm = "arm", transfer = "transfer", drone = "droneLauncher", rSlide = "rightSlide", lSlide = "leftSlide", fr = "rightFront", fl = "leftFront", br = "rightBack", bl = "leftBack", hand = "hand", intake = "intake", imu = "imu";
 
-    //Intake Pos (High Basket)
-    public static final double grabSam = 0.0, relSam = 0.0;
-    //Intake Pos (Bar)
-    public static final double grabSpe = 0.0, relSpe = 0.0;
+    //Intake Pos
+    public static final double grabInt = 0.0, relInt = 0.0;
     //Outtake Pos
-    public static final double outSam = 0.0, outSpe = 0.0;
+    public static final double grabOut = 0.0, relOut = 0.0;
     //Stow Pos
     public static final double intStowIn = 0.0, intStowOut = 0.0, outStowIn = 0.0, outStowOut = 0.0;
 
