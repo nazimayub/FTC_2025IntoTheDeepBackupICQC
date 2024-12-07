e06210faf1cb81ca2802c7a9d3772345d2d3c586
Abdullah Ahmed <abdulahmd@Abdullahs-MacBook-Pro.local>
2024-12-06 19:52:49 -0600

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -17,11 +17,11 @@ public class Constants {
             imu = "imu", otos = "sensor_otos";
 
     //Intake
-    public static double intakeInitTransferPos = .3, intakeFinalTransferPos = .19, intakeDownPos = 0.53, intakeSecondFinalTransferPos = .35,
+    public static double intakeInitTransferPos = .3, intakeFinalTransferPos = .22, intakeDownPos = 0.53, intakeSecondFinalTransferPos = .35,
             block = 0.03, unblock = 0.12;
 
     //Outtake
-    public static double outtakeClawDistTempTransfer = 0.7, outtakeClawDistInitTransfer = .93, outtakeClawDistFinalTransfer = .6, outtakeClawRotTransfer = 0.175,
+    public static double outtakeClawDistTempTransfer = 0.7, outtakeClawDistInitTransfer = .94, outtakeClawDistFinalTransfer = .6, outtakeClawRotTransfer = 0.213,
             grab = 0.22, release = 0.5,
             distBasketPos = .619, rotBasketPos = .194,
             distSpecimenGrab = 0.34, rotSpecimenGrab = .715, distSpecimenGrabFinal = 0.6,
