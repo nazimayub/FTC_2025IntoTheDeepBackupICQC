4fceb6aab05c2ed01a1de5b7f3ca5613989be346
SneakyGolem2 <trex1rstplace@gmail.com>
2025-09-21 13:20:09 -0500

    motortedt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/motortest.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="MyMotorTest", group="Linear Opcode")
+public class motortest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        DcMotor myMotor = hardwareMap.get(DcMotor.class, "motor");
+
+        myMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+
+        while (opModeIsActive()) {
+
+            double motorPower = gamepad1.left_stick_y;
+            myMotor.setPower(motorPower);
+
+            telemetry.addData("Motor Power", myMotor.getPower());
+            telemetry.update();
+        }
+    }
+}
